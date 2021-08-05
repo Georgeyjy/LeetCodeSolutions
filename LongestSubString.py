5ee12c651db342831188e912c4dd6f52ccd36448
Jianyu Yang <yangjianyu@dragonest.com>
2021-08-05 18:27:54 +0800

    longest substring

--- LongestSubString.py
+++ LongestSubString.py
@@ -1,5 +1,43 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2021/8/5 17:06
-# @Author  : Yang Jianyu
-# @File    : LongestSubString.py
-# @Software: LeetCode
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+
+# def lengthOfLongestSubstring(s: str) -> int:
+#     if len(s) == 0:
+#         return 0
+#     if len(s) == 1:
+#         return 1
+#     tmp, res = 1, 0
+#
+#     for i in range(len(s)):
+#         for j in range(i + 1, len(s)):
+#             if s[j] not in s[i:j]:
+#                 tmp += 1
+#             else:
+#                 break
+#         if tmp > res:
+#             res = tmp
+#         tmp = 1
+#
+#     return res
+
+
+def lengthOfLongestSubstring(s: str) -> int:
+    last_idx = {}
+    max_len = 0
+
+    start_idx = 0
+
+    for i in range(0, len(s)):
+
+        if s[i] in last_idx:
+            start_idx = max(start_idx, last_idx[s[i]] + 1)
+
+        max_len = max(max_len, i - start_idx + 1)
+
+        last_idx[s[i]] = i
+
+    return max_len
+
+
+if __name__ == '__main__':
+    print(lengthOfLongestSubstring("abcabcbb"))
