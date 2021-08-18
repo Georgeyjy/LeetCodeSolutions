137b85a7f700e6ad332ec93f7f43b0a6f6b01fa5
Jianyu Yang <yangjianyu@dragonest.com>
2021-08-18 09:21:23 +0800

    Linked list cycle

--- LinkedList/LinkedListCycle.py
+++ LinkedList/LinkedListCycle.py
@@ -3,3 +3,22 @@
 # @Author  : Yang Jianyu
 # @File    : LinkedListCycle.py
 # @Software: LeetCode
+
+
+def hasCycle(self, head: ListNode) -> bool:
+    if not head:
+        return False
+
+    fast = head.next
+    slow = head
+
+    if fast is None or slow is None:
+        return False
+
+    while fast is not None and slow is not None and fast.next is not None:
+        fast = fast.next.next
+        slow = slow.next
+        if fast is slow:
+            return True
+
+    return False
